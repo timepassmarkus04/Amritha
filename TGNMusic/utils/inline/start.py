634c33d7c6a73ddcb7dc3f6d65099e97b2392b23
timepassmarkus04 <markusmarwin2002@gmail.com>
2024-06-11 19:25:13 +0530

    Update start.py

--- TGNMusic/utils/inline/start.py
+++ TGNMusic/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ", url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text="ʙᴏʏ ʙᴇsᴛɪᴇ", url=f"https://t.me/Idhayann"),
+            InlineKeyboardButton(text="ʙᴏʏ ʙᴇsᴛɪᴇ", url=f"https://t.me/daily_grief"),
         ],
     ]
     return buttons
